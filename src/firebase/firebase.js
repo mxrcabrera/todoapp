cbba64c87f416537190331e1eabb270fbab5f1b2
mxrcabrera <73445595+mxrcabrera@users.noreply.github.com>
2024-10-24 02:03:28 -0300

    Fixed code errors

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 import { getDatabase } from 'firebase/database'
 import { getStorage } from 'firebase/storage'
 
